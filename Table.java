5a80d1c65d4d40a696fdb8f642614e5fe0742ec9
Sripriyacse30 <132121681+Sripriyacse30@users.noreply.github.com>
2024-11-27 13:06:30 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,39 @@
+package day4;
+
+
+
+public class Table {
+public static void main(String[] args) {
+	
+	
+	for(int num=1;num<=100;num++) {
+		
+		if(num%3==0) {
+	    System.out.println("Priya");
+		
+	}
+	
+	else {
+		System.out.println(num);
+	}
+}
+ 
+	for(int i=1;i<=100;i++) {
+		if(i%2==0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println(i);
+		}
+	}
+		
+	for(int i=1;i<=100;i++) {
+		if(i%2!=0) {
+			System.out.println("odd");
+		}
+		else {
+			System.out.println(i);
+		}
+	}
+		
+}}
